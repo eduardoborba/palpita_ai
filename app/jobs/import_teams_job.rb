7e6aedca160072644096f6de4b34e5270244d21a
Eduardo Borba <eduardo@jobscore.com>
2020-03-16 00:21:01 -0300

    Create script to import teams

---
+++ app/jobs/import_teams_job.rb
@@ -0,0 +1,24 @@
+class ImportTeamsJob < ApplicationJob
+  FILEPATH = Rails.root.join('vendor', 'teams_data.xml')
+
+  def perform
+    teams_data_xml.xpath("//C").each do |team|
+      next if Team.exists?(name: team["n"])
+
+      Team.create!(
+        name: team["n"],
+        logo_url: team["i"]
+      )
+    end
+  end
+
+  private
+
+  def teams_data_xml
+    @teams_data_xml ||= Nokogiri::XML(file)
+  end
+
+  def file
+    @file ||= File.read(FILEPATH)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_031822) do
+ActiveRecord::Schema.define(version: 2020_03_16_030914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_031822) do
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "logo_url"
   end
 
 end
