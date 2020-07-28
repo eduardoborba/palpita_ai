1332575299cfd89bd979cccee68fa0232ff18c8b
Eduardo Borba <eduardo@jobscore.com>
2020-07-27 22:10:55 -0300

    Create bets page

--- app/controllers/player_round_assignments_controller.rb
+++ app/controllers/player_round_assignments_controller.rb
@@ -14,7 +14,21 @@ class PlayerRoundAssignmentsController < ApplicationController
 
   # GET /player_round_assignments/new
   def new
-    @player_round_assignment = PlayerRoundAssignment.new
+    @round = Round.find(params[:round_id])
+    @bet_league = @round.bet_league
+    @player_round_assignment = PlayerRoundAssignment.new(
+      round: @round,
+      player: current_player,
+      bet_league: @bet_league
+    )
+
+    @player_round_assignment.bets = @round.games.map do |game|
+      Bet.new(
+        game: game,
+        player: current_player,
+        bet_league: @bet_league
+      )
+    end
   end
 
   # GET /player_round_assignments/1/edit
@@ -24,12 +38,16 @@ class PlayerRoundAssignmentsController < ApplicationController
   # POST /player_round_assignments
   # POST /player_round_assignments.json
   def create
-    @player_round_assignment = PlayerRoundAssignment.new(player_round_assignment_params)
+    @player_round_assignment = PlayerRoundAssignment.new
+    @player_round_assignment.attributes = player_round_assignment_params
+
+    @round = @player_round_assignment.round
+    @bet_league = @player_round_assignment.bet_league
 
     respond_to do |format|
       if @player_round_assignment.save
-        format.html { redirect_to @player_round_assignment, notice: 'Player round assignment was successfully created.' }
-        format.json { render :show, status: :created, location: @player_round_assignment }
+        format.html { redirect_to @bet_league, notice: 'Palpite salvo com sucesso.' }
+        format.json { render :show, status: :created, location: @bet_league }
       else
         format.html { render :new }
         format.json { render json: @player_round_assignment.errors, status: :unprocessable_entity }
@@ -42,8 +60,8 @@ class PlayerRoundAssignmentsController < ApplicationController
   def update
     respond_to do |format|
       if @player_round_assignment.update(player_round_assignment_params)
-        format.html { redirect_to @player_round_assignment, notice: 'Player round assignment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @player_round_assignment }
+        format.html { redirect_to @bet_league, notice: 'Palpite salvo com sucesso.' }
+        format.json { render :show, status: :created, location: @bet_league }
       else
         format.html { render :edit }
         format.json { render json: @player_round_assignment.errors, status: :unprocessable_entity }
@@ -62,13 +80,20 @@ class PlayerRoundAssignmentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_player_round_assignment
-      @player_round_assignment = PlayerRoundAssignment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def player_round_assignment_params
-      params.require(:player_round_assignment).permit(:bet_league_id, :player_id, :round_id)
-    end
+  def set_player_round_assignment
+    @player_round_assignment = PlayerRoundAssignment.find(params[:id])
+    @round = @player_round_assignment.round
+    @bet_league = @player_round_assignment.bet_league
+  end
+
+  def player_round_assignment_params
+    params.require(:player_round_assignment).permit(
+      :bet_league_id, :player_id, :round_id,
+      bets_attributes: [
+        :id, :game_id, :player_id, :bet_league_id,
+        :player_round_assignment_id, :home_bet, :visitor_bet
+      ]
+    )
+  end
 end
--- app/helpers/bet_leagues_helper.rb
+++ app/helpers/bet_leagues_helper.rb
@@ -1,2 +1,18 @@
 module BetLeaguesHelper
+  def round_row_url(round)
+    if round.unstarted?
+      if @bet_league.owner == current_player
+        edit_round_path(round)
+      else
+        '#self'
+      end
+    elsif round.started?
+      if current_player.player_round_assignments.exists?(round: round)
+        edit_player_round_assignment_path(current_player.player_round_assignments.find_by(round: round))
+      else
+        new_player_round_assignment_path(round_id: round.id)
+      end
+    else
+    end
+  end
 end
