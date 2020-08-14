ed3802d4ab2e060d1ccb7f78438535b7daf83c85
Eduardo Borba <eduardo@jobscore.com>
2020-08-13 22:16:53 -0300

    Improve logic to show round and finish round

--- app/helpers/bet_leagues_helper.rb
+++ app/helpers/bet_leagues_helper.rb
@@ -12,14 +12,12 @@ module BetLeaguesHelper
         new_player_round_assignment_path(round_id: round.id)
       end
     when 'closed'
-      if league_owner?
+      if current_player.player_round_assignments.exists?(round: round)
+        round_path(round)
+      elsif league_owner?
         edit_finish_round_path(round)
-      elsif current_player.player_round_assignments.exists?(round: round)
-        player_round_assignment_path(
-          current_player.player_round_assignments.find_by(round: round)
-        )
       else
-        '#'
+        '#self'
       end
     when 'finished'
       round_path(round)
