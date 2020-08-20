ef065f79cce95e653313c4585c88824ad6ff145d
Eduardo Borba <eduardo@jobscore.com>
2020-08-19 21:59:37 -0300

    Fix some redirects on the login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
     new_player_session_path
   end
 
+  def after_sign_in_path_for(resource)
+    home_index_path
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,3 +1,11 @@
 class LandingPageController < ApplicationController
+  before_action :check_signed_in
+
   def index; end
+
+  private
+
+  def check_signed_in
+    redirect_to home_index_path if signed_in?
+  end
 end
