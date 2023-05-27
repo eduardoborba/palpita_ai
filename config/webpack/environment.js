7a4b47ff09946c12b4bc077972efb1ce5a3a2492
Eduardo Borba <eduardojoseborba@gmail.com>
2023-05-27 17:32:09 -0300

    upgrade frontend stack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,4 +7,8 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default']
 }))
 
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
 module.exports = environment
