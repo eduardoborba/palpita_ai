f35cbdb3de1be8ecd685425e9d70bc17c967e023
Eduardo Borba <eduardojoseborba@gmail.com>
2023-06-04 21:32:02 -0300

    Add migration to make players lockable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_02_020059) do
+ActiveRecord::Schema.define(version: 2023_06_05_001453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,9 +130,13 @@ ActiveRecord::Schema.define(version: 2023_06_02_020059) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.datetime "locked_at"
+    t.string "unlock_token"
     t.index ["confirmation_token"], name: "index_players_on_confirmation_token", unique: true
     t.index ["email"], name: "index_players_on_email", unique: true
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_players_on_unlock_token", unique: true
   end
 
   create_table "rounds", force: :cascade do |t|
