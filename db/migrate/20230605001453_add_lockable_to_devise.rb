364c84bed1eee3c36ca77d4ec32f677d24d77ead
Eduardo Borba <eduardojoseborba@gmail.com>
2023-06-04 21:34:12 -0300

    Add migration to make players lockable

---
+++ db/migrate/20230605001453_add_lockable_to_devise.rb
@@ -0,0 +1,10 @@
+class AddLockableToDevise < ActiveRecord::Migration[6.1]
+  def change
+    add_column :players, :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :players, :locked_at, :datetime
+
+    # Add these only if unlock strategy is :email or :both
+    add_column :players, :unlock_token, :string
+    add_index :players, :unlock_token, unique: true
+  end
+end
