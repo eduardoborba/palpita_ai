c5bd1b626d90bd10ef9a97a19dd797f6f29e910c
Eduardo Borba <eduardojoseborba@gmail.com>
2023-07-01 21:24:20 -0300

    Add adding integration tests

---
+++ test/system/create_round_test.rb
@@ -0,0 +1,15 @@
+require "application_system_test_case"
+
+class CreateRoundTest < ApplicationSystemTestCase
+  test "create round successfully" do
+    sign_in players(:obina)
+
+    visit "/bet_leagues/#{bet_leagues(:camp_do_obina).id}"
+
+    assert_selector "h1", text: "Camp do Obina"
+
+    click_on "Nova Rodada"
+
+    assert_selector "h1", text: "Rodada"
+  end
+end
