b140fbf773de0a84638db59e252eb73e15aa1f8a
Eduardo Borba <eduardojoseborba@gmail.com>
2023-07-04 18:18:12 -0300

    Test round creation

--- test/system/create_round_test.rb
+++ test/system/create_round_test.rb
@@ -3,6 +3,10 @@
 require 'application_system_test_case'
 
 class CreateRoundTest < ApplicationSystemTestCase
+  setup do
+    bet_leagues(:camp_do_obina).rounds.destroy_all
+  end
+
   test 'create round successfully' do
     sign_in players(:obina)
 
@@ -13,5 +17,20 @@ class CreateRoundTest < ApplicationSystemTestCase
     click_on 'Nova Rodada'
 
     assert_selector 'h1', text: 'Rodada'
+
+    find('button[data-id="round_games_attributes_0_home_id"]').click
+    assert_selector '.dropdown-menu li', text: 'Flamengo'
+    find('.dropdown-menu li', text: 'Flamengo').click
+
+    find('button.dropdown-toggle[data-id="round_games_attributes_0_visitor_id"]').click
+    assert_selector '.dropdown-menu li', text: 'Vasco da Gama'
+    find('.dropdown-menu li', text: 'Vasco da Gama').click
+
+    click_on 'Criar'
+
+    assert_selector 'div.alert', text: 'Rodada foi criada com sucesso.'
+
+    assert_selector '[data-controller="rounds-list"] .list-group-item', text: 'Rodada #1'
+    assert_selector '[data-controller="rounds-list"] .list-group-item', text: '1 jogo'
   end
 end
