7062c8dd6e08d1e05fa29876b3014b556366fef1
Eduardo Borba <eduardojoseborba@gmail.com>
2023-07-04 17:32:10 -0300

    Satisfy rubocop

--- test/system/create_round_test.rb
+++ test/system/create_round_test.rb
@@ -1,15 +1,17 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class CreateRoundTest < ApplicationSystemTestCase
-  test "create round successfully" do
+  test 'create round successfully' do
     sign_in players(:obina)
 
     visit "/bet_leagues/#{bet_leagues(:camp_do_obina).id}"
 
-    assert_selector "h1", text: "Camp do Obina"
+    assert_selector 'h1', text: 'Camp do Obina'
 
-    click_on "Nova Rodada"
+    click_on 'Nova Rodada'
 
-    assert_selector "h1", text: "Rodada"
+    assert_selector 'h1', text: 'Rodada'
   end
 end
